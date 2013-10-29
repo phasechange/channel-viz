68e85427304baf80e6f48194f0de4dd49c84df8f
phasechange <michael@phasechangedesign.com>
2013-10-29 15:35:59 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -15,8 +15,8 @@
 	var defaultKey		= '84KOAL0AQ2mG6mhUdVpACUCgwnjETVD61jL1tf5xLkwj2yX8', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1206868648], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Craftsbury Ice House', // Replaces Xively logo in the header
-		dataDuration	= '12hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 30, // Default interval for data to be displayed (in seconds)
+		dataDuration	= '6hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '69ab35', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
