09e6b7e56bff16c751f1f73a4328285638eb8827
phasechange <michael@phasechangedesign.com>
2013-10-29 21:06:52 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -160,6 +160,16 @@
 										series: series
 									});
 
+var legend = new Rickshaw.Graph.Legend({
+    graph: graph,
+    element: document.querySelector('#graph')
+});
+
+var shelving = new Rickshaw.Graph.Behavior.Series.Toggle({
+    graph: graph,
+    legend: legend
+});
+
 									graph.render();
 									
 
